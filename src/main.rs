f9ea019e62bb38c0f1b05963db9ec14fe7734c18
stormofice <58337328+stormofice@users.noreply.github.com>
2023-02-24 18:03:37 +0100

    feat: Iterate over all available entries

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
+use std::mem::transmute;
 
-#[repr(C)]
 struct DatabaseHeader {
     magic: [u8; 4],
     version: u32,
@@ -10,7 +10,7 @@ struct DatabaseHeader {
     available_entry_offset: u32,
 }
 
-#[repr(C)]
+#[repr(C, packed)]
 struct CacheEntry {
     magic: [u8; 4],
     entry_size: u32,
@@ -72,6 +72,10 @@ impl TryFrom<u32> for CacheType {
 fn main() -> std::io::Result<()> {
     let contents = fs::read("./thumbcache_1280.db")?;
 
+    println!("File size: {}", contents.len());
+    println!("Database header size: {}", std::mem::size_of::<DatabaseHeader>());
+    println!("Cache entry size: {}", CACHE_ENTRY_SIZE);
+
     let (prefix, header, suffix) = unsafe {
         contents.align_to::<DatabaseHeader>()
     };
@@ -92,27 +96,28 @@ fn main() -> std::io::Result<()> {
     assert_eq!(header.magic, [0x43, 0x4D, 0x4D, 0x4D]);
 
     // Offset until now: 24 bytes
-    let mut offset = 24;
+    let mut offset = header.first_entry_offset as usize;
 
-    for index in 0..1 {
-        let (_, entry, _) = unsafe {
-            contents[offset..].align_to::<CacheEntry>()
+    while offset < contents.len()  {
+        println!("Entry at offset {}",  offset);
+
+        let entry = unsafe {
+            transmute::<[u8; CACHE_ENTRY_SIZE],
+                CacheEntry>(contents[offset..offset + CACHE_ENTRY_SIZE]
+                .try_into().expect("Invalid cache entry"))
         };
 
-        let entry = &entry[0];
-
-        println!("Entry #{}", index);
-        println!("Magic: {:?}", entry.magic);
-        println!("Entry size: {}", entry.entry_size);
-        println!("Hash: {:x}", entry.hash);
-        println!("Filename length: {}", entry.filename_length);
-        println!("Padding size: {}", entry.padding_size);
-        println!("Data size: {}", entry.data_size);
-        println!("Width: {}", entry.width);
-        println!("Height: {}", entry.height);
-        println!("Unknown: {}", entry.unknown);
-        println!("Data checksum: {:x}", entry.data_checksum);
-        println!("Header checksum: {:x}", entry.header_checksum);
+        println!("Magic: {:?}", { entry.magic });
+        println!("Entry size: {}", { entry.entry_size });
+        println!("Hash: {:x}", { entry.hash });
+        println!("Filename length: {}", { entry.filename_length });
+        println!("Padding size: {}", { entry.padding_size });
+        println!("Data size: {}", { entry.data_size });
+        println!("Width: {}", { entry.width });
+        println!("Height: {}", { entry.height });
+        println!("Unknown: {}", { entry.unknown });
+        println!("Data checksum: {:x}", { entry.data_checksum });
+        println!("Header checksum: {:x}", { entry.header_checksum });
 
         assert_eq!(entry.magic, [0x43, 0x4D, 0x4D, 0x4D]);
 
@@ -126,9 +131,9 @@ fn main() -> std::io::Result<()> {
         let data_start = offset + CACHE_ENTRY_SIZE + entry.filename_length as usize + entry.padding_size as usize;
         let data_end = data_start + entry.data_size as usize;
         let data = &contents[data_start..data_end];
-        println!("Data: {:x?}", data);
+        //println!("Data: {:x?}", data);
 
-        fs::write("sample.jpg", data).expect("Unable to write file");
+        //fs::write("sample.jpg", data).expect("Unable to write file");
 
         offset += entry.entry_size as usize;
     }
